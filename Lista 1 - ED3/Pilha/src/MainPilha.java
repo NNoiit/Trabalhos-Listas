71ea7ff889010a22afc5a3e8c12db488be7ce536
Cristion A.Assis <57966693+NNoiit@users.noreply.github.com>
2022-10-25 14:11:51 -0300

    Q1 reformulada

--- Lista 1 - ED3/Pilha/src/MainPilha.java
+++ Lista 1 - ED3/Pilha/src/MainPilha.java
@@ -5,50 +5,79 @@ public class MainPilha
 	public static void main(String args[])
 	{
 		Scanner scanner = new Scanner(System.in);
-		
-		Pilha pilha1 = new Pilha(20);
-		Pilha pilha2 = new Pilha(20);
-		String s;
-		char c;
-		
+		int opc = scanner.next();
+
+		switch(opc){
+			case 1:
+			
+				Pilha pilha1 = new Pilha(20);
+				Pilha pilha2 = new Pilha(20);
+				String s;
+				char c;
 				
-		System.out.println("Entre com uma frase: ");
-		s = scanner.nextLine();
+						
+				System.out.println("Entre com uma frase: ");
+				s = scanner.nextLine();
+				
+				int i = 0, x = 0;
+				
+				while (i < s.length())
+				{
+					c = s.charAt(i);
+					if((i == 'A'|| i == 'B') && x == 0){
+						pilha1.push(c);
+						i++;
+					} else if((i == 'A'|| i == 'B') && x == 1){
+						pilha2.push(c);
+						i--;
+					}
 		
-		int i = 0, x = 0;
-		
-		while (i < s.length())
-		{
-			c = s.charAt(i);
-			if(i < (s.length()-1)/2){
-				pilha1.push(c);
-				i++;
-			}
-			if(i > (s.length()-1)/2){
-				pilha2.push(c);
-				i--;
-			}
-
-			if((c != 'A' || c != 'B')){
-				if(x==1 && (c != 'A' || c != 'B')){
-					break;
+					if(((c != 'A' || c != 'B'))){
+						if(x==1 && (c != 'A' || c != 'B')){
+							break;
+						}
+						x = 1;
+						i = s.length()-1;
+					}
 				}
-				x = 1;
-				i = s.length()-1;
+				
+				if(verifica_inverso(pilha1, pilha2)){
+					Sistem.out.println("Correto");
+				} else{
+					Sistem.out.println("Incorreto");
+				}
+				break;
+
+			case 2:
+
+				Pilha pilha1_q2 = new Pilha(20);
+
+				int i = 0, x = 0;
+				
+				while (i < s.length())
+				{
+					c = s.charAt(i);
+					pilha1_q2.push(c);
+					i++;
+
+					if(c == 'D'){
+					}
+				}
+
+
 			}
 
+			scanner.close();
+			
 		}
-		
-		verifica_inverso(pilha1, pilha2);
-		
-		scanner.close();
-	}
 
+	
+	
 	//Q - 1 Verificação de contrario
 	public static boolean verifica_inverso(Pilha pilha1, Pilha pilha2){
 		char x, y;
 		
-		while((!pilha1.vazia())){
+		while(!pilha1.vazia() || !pilha2.vazia()){
 
 			x = pilha1.pop();
 			y = pilha2.pop();
@@ -58,13 +87,14 @@ public class MainPilha
 			}
 
 			if(x != 'A'|| x != 'B'|| y != 'A'|| y != 'B'){
-				break;
+				return false;
 			}
 		}
 
 		return true;
 	}
 
+	//Q - 2 Verificação formato aDbDc
 	public boolean verifica_cadeia(Pilha pilha){
 
 		while(pilha.vazia()){
